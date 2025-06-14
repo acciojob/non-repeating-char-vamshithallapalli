c43021b2364e10ce172434098c98c2925b702e70
Vamshi Thallapalli <127075912+vamshithallapalli@users.noreply.github.com>
2025-06-14 13:25:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	const count = new Map();
+
+	for(let char of str){
+		count.set(char, (count.get(char) || 0)+1);
+	}
+
+	for(let char of str){
+		if(count.get(char) === 1){
+			return char;
+		}
+	}
+	return null;
+
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
